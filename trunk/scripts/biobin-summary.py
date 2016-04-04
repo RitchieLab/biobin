8b39c8c61829b31472ba72e994c47e3751f92d36
atf3 <atf3@psu.edu>
2016-04-04 19:51:28 +0000

    new case and control capacity columns for the summary output
    git-svn-id: https://svn.ritchielab.psu.edu/svn/projects/biobin@4125 9e62ecf6-770d-0410-a9d7-e62a33e3d092

--- trunk/scripts/biobin-summary.py
+++ trunk/scripts/biobin-summary.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
 		ctl_vars=f.readline().strip().split(args.sep)[2:]
 		case_vars=f.readline().strip().split(args.sep)[2:]
 		
-		f.readline()
-		f.readline()
+		ctl_cap=f.readline().strip().split(args.sep)[2:]
+		case_cap=f.readline().strip().split(args.sep)[2:]
 		
 		# OK, now the header is read, let's read the p-values
 		
@@ -46,7 +46,7 @@ if __name__ == "__main__":
 			if pv_arr[0].endswith("p-value"):
 				pv_test = pv_arr[0].rsplit(" ", 1)
 				if not header_printed:
-					pv_names.add(pv_test[0])
+					pv_names.add(pv_test[0].replace('-','_'))
 				pv_dict[pv_test[0]] = pv_arr[2:]
 			else:
 				n_case += (pv_arr[1] != "nan" and (not args.all_control) and pv_arr[1] != args.control_value)
@@ -68,10 +68,36 @@ if __name__ == "__main__":
 		# First print the header
 		if not header_printed:
 			header_printed = True
-			print args.output_sep.join(["Phenotype", "Bin", "Num_Cases", "Num_Controls", "Num_Loci", "Num_Vars", "Case_Vars", "Control_Vars"] + [n for n in pv_names])
+			print args.output_sep.join(
+				[
+					"Outcome",
+					"Bin",
+					"Num_Cases",
+					"Num_Controls",
+					"Num_Loci",
+					"Num_Vars",
+					"Case_Vars",
+					"Control_Vars",
+					"Case_Capacity",
+					"Control_Capacity"
+				] + [n for n in pv_names]
+			)
 		
 		# Now, enumerate all the lists we have - they should have the same # of elements
 		for i in xrange(len(bin_names)):
-			print args.output_sep.join([pheno_name, bin_names[i], n_case, n_control, bin_loci[i], bin_vars[i], case_vars[i], ctl_vars[i]] + [pv_dict[n][i] for n in pv_names])
+			print args.output_sep.join(
+				[
+					pheno_name,
+					bin_names[i],
+					n_case,
+					n_control,
+					bin_loci[i],
+					bin_vars[i],
+					case_vars[i],
+					ctl_vars[i],
+					case_cap[i],
+					ctl_cap[i]
+				] + [pv_dict[n][i] for n in pv_names]
+			)
 		
 	
